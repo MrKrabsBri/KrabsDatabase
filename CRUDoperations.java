84146e54b122a6a820cd5266180932be11a9b73f
Julius Bri <Julius Bri>
2023-09-14 14:23:08 +0300

    creating READ operation and pooling connections

--- CRUDoperations.java
+++ CRUDoperations.java
@@ -1,39 +1,41 @@
 package com.krabs;
 
+import com.mysql.cj.protocol.Resultset;
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class CRUDoperations {
 
-public static void createOperation(Connection connection,String username,String email,String password ){
+    public static void createOperation(Connection connection,String username,String email,String password ){
 
-    try {
-        String insertQuery = "INSERT INTO users (username, email, password) VALUES (?, ?, ?)";
+        try {
+            String insertQuery = "INSERT INTO users (username, email, password) VALUES (?, ?, ?)";
 
+            try (PreparedStatement preparedStatement = connection.prepareStatement(insertQuery)) {
+                preparedStatement.setString(1, username);
+                preparedStatement.setString(2, email);
+                preparedStatement.setString(3, password);
+                int rowsInserted = preparedStatement.executeUpdate();
 
-
-        try (PreparedStatement preparedStatement = connection.prepareStatement(insertQuery)) {
-            preparedStatement.setString(1, username);
-            preparedStatement.setString(2, email);
-            preparedStatement.setString(3, password);
-            int rowsInserted = preparedStatement.executeUpdate();
-
-            if (rowsInserted > 0) {
-                System.out.println("A new employee was inserted successfully.");
-            } else {
-                System.out.println("Insertion failed.");
+                if (rowsInserted > 0) {
+                    System.out.println("A new employee was inserted successfully.");
+                } else {
+                    System.out.println("Insertion failed.");
+                }
             }
-        }// catch (SQLException e) {
-//            throw new RuntimeException(e);
-//        }
-    } catch (SQLException e) {
-        e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
+    public static void readOperation(Connection connection){ //select
 
+        String Query = "SELECT * FROM employees";
+        Resultset resultSet = statement.executeQuery(query);
+    }
 
-}
 
 
 }
